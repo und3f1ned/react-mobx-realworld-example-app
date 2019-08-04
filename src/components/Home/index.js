672c7b7c5665834d019fd0b0f6c602b5e636d1c5
und3f1ned <dev@xedin.ru>
2019-08-04 12:59:03 +0300

    old fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,11 @@
-import Banner from './Banner';
-import MainView from './MainView';
-import React from 'react';
-import Tags from './Tags';
-import { inject, observer } from 'mobx-react';
-import { withRouter } from 'react-router-dom';
+import Banner from "./Banner";
+import MainView from "./MainView";
+import React from "react";
+import Tags from "./Tags";
+import { inject, observer } from "mobx-react";
+import { withRouter } from "react-router-dom";
 
-@inject('commonStore')
+@inject("commonStore")
 @withRouter
 @observer
 export default class Home extends React.Component {
@@ -15,30 +15,6 @@ export default class Home extends React.Component {
 
   render() {
     const { tags, token, appName } = this.props.commonStore;
-    return (
-      <div className="home-page">
-
-        <Banner token={token} appName={appName} />
-
-        <div className="container page">
-          <div className="row">
-            <MainView />
-
-            <div className="col-md-3">
-              <div className="sidebar">
-
-                <p>Popular Tags</p>
-
-                <Tags
-                  tags={tags}
-                />
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    );
+    return <div className="home-page">111</div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import ReactDOM from 'react-dom';
-import promiseFinally from 'promise.prototype.finally';
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
-import { useStrict } from 'mobx';
-import { Provider } from 'mobx-react';
+import ReactDOM from "react-dom";
+import promiseFinally from "promise.prototype.finally";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { configure } from "mobx";
+import { Provider } from "mobx-react";
 
-import App from './components/App';
+import App from "./components/App";
 
-import articlesStore from './stores/articlesStore';
-import commentsStore from './stores/commentsStore';
-import authStore from './stores/authStore';
-import commonStore from './stores/commonStore';
-import editorStore from './stores/editorStore';
-import userStore from './stores/userStore';
-import profileStore from './stores/profileStore';
+import articlesStore from "./stores/articlesStore";
+import commentsStore from "./stores/commentsStore";
+import authStore from "./stores/authStore";
+import commonStore from "./stores/commonStore";
+import editorStore from "./stores/editorStore";
+import userStore from "./stores/userStore";
+import profileStore from "./stores/profileStore";
 
 const stores = {
   articlesStore,
@@ -29,12 +29,13 @@ const stores = {
 window._____APP_STATE_____ = stores;
 
 promiseFinally.shim();
-useStrict(true);
+configure({ enforceActions: "always" });
 
-ReactDOM.render((
+ReactDOM.render(
   <Provider {...stores}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
-  </Provider>
-), document.getElementById('root'));
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
